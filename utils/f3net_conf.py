778b5b1d1afc605c880f6b7d52d63263a201722e
ethaiyi9 <yiceyuan@126.com>
2023-12-09 16:33:20 +0800

    修改导致现在可以直接在服务器上跑

--- utils/f3net_conf.py
+++ utils/f3net_conf.py
@@ -27,15 +27,25 @@ dfdc_root = r"/hy-tmp/Dfdc"
 
 model_path_name = r"G:\我的云端硬盘\models\F3\test_6(git_version)\model2.pth"
 xception_pretrained_path =r"D:\DeepFakeProject_in_D\deepfake_project\our_code\f3net\models\xception-b5690688.pth"
-efficient_pretrained_path=r"/hy-nas/F3_Net/utils/deepware.pt"
+efficient_pretrained_path=r"/hy-nas/f3-net/utils/deepware.pt"
 
 dfdc_syn_root=r"/hy-tmp/Dfdc/Dfdc_syn"
 dfdc_real_root=r"/hy-tmp/Dfdc/Dfdc_real"
 dfdc_csv_root=r"/hy-tmp/Dfdc/csv"
 
-ff_real_root = r"/hy-tmp/FF++/FF_real"
-ff_syn_root = r"/hy-tmp/FF++/FF_syn"
-ff_csv_root = r"/hy-tmp/FF++/csv"
+ff_f2f_real_root = r"/hy-tmp/FF_f2f/FF_real"
+ff_f2f_syn_root = r"/hy-tmp/FF_f2f/FF_f2f"
+ff_f2f_csv_root = r"/hy-tmp/FF_f2f/csv"
 
 
+ff_df_real_root = r"/hy-tmp/FF_df/FF_real"
+ff_df_syn_root = r"/hy-tmp/FF_df/FF_df"
+ff_df_csv_root = r"/hy-tmp/FF_df/csv"
 
+ff_fs_real_root = r"/hy-tmp/FF_fs/FF_real"
+ff_fs_syn_root = r"/hy-tmp/FF_fs/FF_fs"
+ff_fs_csv_root = r"/hy-tmp/FF_fs/csv"
+
+ff_nt_real_root = r"/hy-tmp/FF_nt/FF_real"
+ff_nt_syn_root = r"/hy-tmp/FF_nt/FF_nt"
+ff_nt_csv_root = r"/hy-tmp/FF_nt/csv"
